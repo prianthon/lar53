cbb59bad907a54c93498bd6114f188f779462025
prianthon <prianthonsubardio@gmail.com>
2017-12-11 23:38:57 +0700

    model 46

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function() {
+	return '<h1>Halo</h1>'
+		.'Selamat datang di webapp saya<br>'
+		.'Laravel, emang keren.';
+});
+Route::get('/testmodel', function() {
+	$post = new App\Post;
+	$post->title = "7 Amalan Pembuka Jodoh";
+	$post->content = "shalat malam, sedekah, puasa sunah, silaturahmi, senyum, doa, tobat";
+	$post->save();
+	return $post;
+});
